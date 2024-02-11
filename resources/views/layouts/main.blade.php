7de6d37d77f782609993dae5ba9c754e8c393933
NeroznakD <neroznakdn@gmail.com>
2024-02-11 17:01:03 +0100

    Create repository. Version 1.0.0

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -80,8 +80,8 @@
 
     @yield('content')
     <footer class="main-footer">
-        <strong>Copyright &copy; {{date('Y')}} <a href="https://neroznak.com">neroznak.com</a>.</strong>
-        Все права защищены
+{{--        <strong>Copyright &copy; {{date('Y')}} <a href="https://neroznak.com">neroznak.com</a>.</strong>--}}
+{{--        Все права защищены--}}
         <div class="float-right d-none d-sm-inline-block">
             <b>Version</b> 1.0.0
         </div>
