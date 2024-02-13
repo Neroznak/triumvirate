980f85d7b982b97b86d35c2e7c1d15e27fa4eec1
NeroznakD <neroznakdn@gmail.com>
2024-02-13 16:48:23 +0100

    V 1.0.0 machine bread pieces

--- resources/views/machines/show.blade.php
+++ resources/views/machines/show.blade.php
@@ -7,12 +7,12 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Отделы</h1>
+                        <h1 class="m-0">{{$machine->name}}</h1>
                     </div><!-- /.col -->
                     <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Dashboard v1</li>
+                            <li class="breadcrumb-item">Техника</li>
+                            <li class="breadcrumb-item active">{{$machine->name}}</li>
                         </ol>
                     </div><!-- /.col -->
                 </div><!-- /.row -->
